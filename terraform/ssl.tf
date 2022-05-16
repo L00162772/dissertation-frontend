1a8682867f8ed9692e1a8666510e4775055bd3a8
Damien Gallagher <L00162772@student.lyit.ie>
2022-05-17 00:26:12 +0100

    Update

--- terraform/ssl.tf
+++ terraform/ssl.tf
@@ -1,3 +1,4 @@
+# Note SSL certs can only be created in this region
 resource "aws_acm_certificate" "frontend_cloudfront_cert" {
   provider          = aws.east1
   domain_name       = local.domain
@@ -10,7 +11,8 @@ resource "aws_acm_certificate" "frontend_cloudfront_cert" {
 
 
 resource "aws_acm_certificate_validation" "validation" {
-  certificate_arn         = aws_acm_certificate.frontend_cloudfront_cert.arn
+  certificate_arn   = aws_acm_certificate.frontend_cloudfront_cert.arn
+  provider          = aws.east1
   validation_record_fqdns = [for record in aws_route53_record.frontend_cloudfront_route53_validation_record : record.fqdn]
   depends_on = [
     aws_route53_record.frontend_cloudfront_route53_validation_record
