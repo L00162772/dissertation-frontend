7b70f4e6787b4a6e7ce8b284d3f473747489824e
Damien Gallagher <L00162772@student.lyit.ie>
2022-05-17 22:48:01 +0100

    Update

--- scripts/invalidate_cloudfront_distribution.py
+++ scripts/invalidate_cloudfront_distribution.py
@@ -1,6 +1,18 @@
+import os
 import boto3
+
+aws_region = os.environ['AWS_REGION']
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+
+print("aws_region:", aws_region)
+print("aws_access_key_id:", aws_access_key_id)
+print("aws_secret_access_key:", aws_secret_access_key)
 print("In invalidate_cloudfront_distribution")
 
-client = boto3.client('cloudfront')
-response = client.list_distributions()
-print(f"response: {response}")
+client = boto3.client('cloudfront', 
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key)
+print("Here 2")  
+list_distributions_response = client.list_distributions()
+print(f"list_distributions_response: {list_distributions_response}")
