a992839419a92d1e551b7039a9a787a0d04d1141
Damien Gallagher <L00162772@student.lyit.ie>
2022-05-19 00:11:56 +0100

    Update

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -14,8 +14,8 @@ data "aws_iam_policy_document" "s3-website-policy" {
 }
 
 resource "aws_s3_bucket" "frontend_s3_bucket" {
-  bucket        = "${var.aws_region}-${var.bucket_name_postfix}"
-  acl           = "public-read"
+  bucket = "${var.aws_region}-${var.bucket_name_postfix}"
+
   force_destroy = true
 }
 
@@ -30,7 +30,14 @@ resource "aws_s3_bucket_website_configuration" "frontend_s3_bucket_configuration
     key = "index.html"
   }
 }
+resource "aws_s3_bucket_acl" "frontend_s3_bucket_acl" {
+  bucket = aws_s3_bucket.frontend_s3_bucket.id
+  acl    = "public-read"
 
+  depends_on = [
+    aws_s3_bucket.frontend_s3_bucket
+  ]
+}
 
 resource "aws_s3_bucket_public_access_block" "frontend_s3_access_control" {
   bucket             = aws_s3_bucket.frontend_s3_bucket.id
@@ -38,7 +45,7 @@ resource "aws_s3_bucket_public_access_block" "frontend_s3_access_control" {
   ignore_public_acls = true
 }
 
-resource "aws_s3_bucket_policy" "frontend_s3_bucket" {
+resource "aws_s3_bucket_policy" "frontend_s3_bucket_policy" {
   bucket = aws_s3_bucket.frontend_s3_bucket.id
   policy = data.aws_iam_policy_document.s3-website-policy.json
 }
