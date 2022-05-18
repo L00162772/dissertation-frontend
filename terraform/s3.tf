ddd020ad5cd01f774824a9862235569e88ff0f7f
Damien Gallagher <L00162772@student.lyit.ie>
2022-05-18 22:58:39 +0100

    Update

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -17,12 +17,22 @@ resource "aws_s3_bucket" "frontend_s3_bucket" {
   bucket = "${var.aws_region}-${var.bucket_name_postfix}"
   acl    = "public-read"
   policy = data.aws_iam_policy_document.s3-website-policy.json
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
+
   force_destroy = true
 }
+
+resource "aws_s3_bucket_website_configuration" "frontend_s3_bucket" {
+  bucket = aws_s3_bucket.frontend_s3_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_documindexent {
+    key = "error.html"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "frontend_s3_access_control" {
   bucket             = aws_s3_bucket.frontend_s3_bucket.id
   block_public_acls  = true
