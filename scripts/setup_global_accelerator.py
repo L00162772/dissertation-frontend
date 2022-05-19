0ee7a1ded49a074497b17e30ace85b29f58f68ea
Damien Gallagher <L00162772@student.lyit.ie>
2022-05-19 10:33:14 +0100

    Update

--- scripts/setup_global_accelerator.py
+++ scripts/setup_global_accelerator.py
@@ -80,6 +80,7 @@ if not has_frontend_tag:
     str = '/hostedzone/'
     hosted_zone_id = hosted_zone_id[len(str):]
     print(f"hosted_zone_id 2:{hosted_zone_id}")
+    dns_name = f'{aws_region}-alb-frontend.atu-dissertation.com'
     change_resource_record_sets_response = route53_client.change_resource_record_sets(
         HostedZoneId=hosted_zone_id,
         ChangeBatch={
@@ -87,13 +88,13 @@ if not has_frontend_tag:
                 {
                     'Action': 'CREATE',
                     'ResourceRecordSet': {
-                        'Name': 'frontend',
+                        'Name': dns_name,
                         'Type': 'A',
                         'SetIdentifier': 'string',
                         'Region': 'us-east-1',
                         'AliasTarget': {
                             'HostedZoneId': hosted_zone_id,
-                            'DNSName': f'{aws_region}-alb-frontend.atu-dissertation.com',
+                            'DNSName': dns_name,
                             'EvaluateTargetHealth': True
                         }
                     }
