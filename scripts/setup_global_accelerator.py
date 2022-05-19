bf0aa27bfd49a84935124ce98057c53ba5f18e3f
Damien Gallagher <L00162772@student.lyit.ie>
2022-05-19 01:36:44 +0100

    Update

--- scripts/setup_global_accelerator.py
+++ scripts/setup_global_accelerator.py
@@ -15,16 +15,16 @@ print("aws_secret_access_key:", aws_secret_access_key)
 print("In setup global accelerator")
 
 client = boto3.client('globalaccelerator',
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-    region_name=accelerator_region)
+                      aws_access_key_id=aws_access_key_id,
+                      aws_secret_access_key=aws_secret_access_key,
+                      region_name=accelerator_region)
 
 route53_client = boto3.client('route53',
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-    region_name=base_region)
-    
-print("Here 2")  
+                              aws_access_key_id=aws_access_key_id,
+                              aws_secret_access_key=aws_secret_access_key,
+                              region_name=base_region)
+
+print("Here 2")
 list_accelerators_response = client.list_accelerators()
 print(f"list_accelerators_response: {list_accelerators_response}")
 
@@ -35,7 +35,8 @@ for accelerator in list_accelerators_response['Accelerators']:
     accelerator_arn = accelerator['AcceleratorArn']
     print(f" accelerator_arn: {accelerator_arn}")
 
-    tags_for_resource_response = client.list_tags_for_resource(Resource=accelerator_arn)
+    tags_for_resource_response = client.list_tags_for_resource(
+        Resource=accelerator_arn)
     print(f"tags_for_resource_response: {tags_for_resource_response}")
 
     has_frontend_tag = False
@@ -45,55 +46,55 @@ for accelerator in list_accelerators_response['Accelerators']:
         value = tag['Value']
         if key == 'Name' and value.lower() == 'frontend':
             has_frontend_tag = True
-        
+
         print(f" has_frontend_tag: {has_frontend_tag}")
 
-    
     if has_frontend_tag:
         break
-    
+
 if not has_frontend_tag:
-        create_accelerator_response = client.create_accelerator(
-            Name='frontend',
-            IpAddressType='IPV4',
-            Enabled=True,
-            Tags=[
+    create_accelerator_response = client.create_accelerator(
+        Name='frontend',
+        IpAddressType='IPV4',
+        Enabled=True,
+        Tags=[
+            {
+                'Key': 'Name',
+                'Value': 'frontend'
+            },
+        ]
+    )
+    print(f"create_accelerator_response: {create_accelerator_response}")
+
+    hosted_zones_response = route53_client.list_hosted_zones()
+    print(f"hosted_zones_response: {hosted_zones_response}")
+    for hosted_zone in hosted_zones_response['HostedZones']:
+        print(f"hosted_zone: {hosted_zone}")
+        if hosted_zone['Name'].lower() == base_dns:
+            hosted_zone_id = hosted_zone['Id']
+
+    print(f"hosted_zone_id :{hosted_zone_id}")
+    change_resource_record_sets_response = route53_client.change_resource_record_sets(
+        HostedZoneId=hosted_zone_id,
+        ChangeBatch={
+            'Changes': [
                 {
-                    'Key': 'Name',
-                    'Value': 'frontend'
+                    'Action': 'CREATE',
+                    'ResourceRecordSet': {
+                        'Name': 'frontend',
+                        'Type': 'A',
+                        'SetIdentifier': 'string',
+                        'Region': 'us-east-1',
+                        'TTL': 60,
+                        'AliasTarget': {
+                            'HostedZoneId': hosted_zone_id,
+                            'DNSName': 'string',
+                            'EvaluateTargetHealth': True
+                        }
+                    }
                 },
             ]
-        )
-        print(f"create_accelerator_response: {create_accelerator_response}")
-
-        hosted_zones_response = route53_client.list_hosted_zones()
-        print(f"hosted_zones_response: {hosted_zones_response}")
-        for hosted_zone in hosted_zones_response['HostedZones']:
-            print(f"hosted_zone: {hosted_zone}")
-            if hosted_zone['Name'].lower() == base_dns:
-                hosted_zone_id = hosted_zone['Id']
-        
-        print(f"hosted_zone_id :{hosted_zone_id}")
-        change_resource_record_sets_response = route53_client.change_resource_record_sets(
-                        HostedZoneId=hosted_zone_id,
-                        ChangeBatch={
-                            'Changes': [
-                                {
-                                    'Action': 'CREATE',
-                                    'ResourceRecordSet': {
-                                        'Name': 'frontend',
-                                        'Type': 'A',
-                                        'SetIdentifier': 'string',
-                                        'Region': 'us-east-1',
-                                        'TTL': 60,
-                                        'AliasTarget': {
-                                            'HostedZoneId': hosted_zone_id,
-                                            'DNSName': 'string',
-                                            'EvaluateTargetHealth': True
-                                        }
-                                    }
-                                },
-                            ]
-                        }
-                    )
-        print(f"change_resource_record_sets_response: {create_accelerator_response}")
+        }
+    )
+    print(
+        f"change_resource_record_sets_response: {create_accelerator_response}")
