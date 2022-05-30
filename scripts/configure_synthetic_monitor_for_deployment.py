413ca2811ba6a263bc351d46f4d1993e801d23e0
Damien Gallagher <L00162772@student.lyit.ie>
2022-05-31 00:37:00 +0100

    Update

--- scripts/configure_synthetic_monitor_for_deployment.py
+++ scripts/configure_synthetic_monitor_for_deployment.py
@@ -19,14 +19,20 @@ client = boto3.client('synthetics',
 synthetic_monitor_name = f'{application_type}_canary'
 print(f"synthetic_monitor_name:{synthetic_monitor_name}")
 
+get_canary_response = client.get_canary(Name=synthetic_monitor_name)
+print(f"get_canary_response:{get_canary_response}")
+
+canary_state = get_canary_response['Canary']['Status']['State']
+print(f"canary_state:{canary_state}")
+
 print(f"start_synthetic_monitor:{start_synthetic_monitor}")
 if start_synthetic_monitor.lower() == "true":
-    print("Starting the synthetic monitor")
-    start_canary_response = client.start_canary(Name=synthetic_monitor_name)
-
-    print(f"start_canary_response:{start_canary_response}")
+    if canary_state == 'STOPPED':
+        print("Starting the synthetic monitor")
+        start_canary_response = client.start_canary(Name=synthetic_monitor_name)
+        print(f"start_canary_response:{start_canary_response}")
 else:
-    print("Stopping the synthetic monitor")
-    stop_canary_response = client.stop_canary(Name=synthetic_monitor_name)
-
-    print(f"stop_canary_response:{stop_canary_response}")
+    if canary_state == 'RUNNING':
+        print("Stopping the synthetic monitor")
+        stop_canary_response = client.stop_canary(Name=synthetic_monitor_name)
+        print(f"stop_canary_response:{stop_canary_response}")
