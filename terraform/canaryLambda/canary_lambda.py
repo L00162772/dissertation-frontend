72b30200e49464fd255d41c17bc62f32d347dad7
Damien Gallagher <damien.gallagher@gmail.com>
2022-05-31 10:13:04 +0100

    Updating code

--- terraform/canaryLambda/canary_lambda.py
+++ terraform/canaryLambda/canary_lambda.py
@@ -1,8 +1,13 @@
-def lambda_handler(event, context):
-   print(f"event:{event}")
-   print(f"context:{context}")
-   message = 'Hello {} !'.format(event['key1'])
+from cgi import test
 
-   return {
-       'message' : message
-   }
+
+def lambda_handler(event, context):
+    print(f"event:{event}")
+
+    test_run_status = event['detail']['test-run-status']
+    print(f"test_run_status:{test_run_status}")
+   
+    if test_run_status.lower() == "failed":
+        print("Test failed - remove region from global accelerator")
+    else:
+        print("Test Passed - verify region is part of global accelerator")
