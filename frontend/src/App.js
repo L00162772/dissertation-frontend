92394cc7d00cb29839c6292dc66c8de062b784b2
Damien Gallagher <L00162772@student.lyit.ie>
2022-06-03 22:08:31 +0100

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,34 @@ import Create from './components/create';
 import Read from './components/read';
 import Update from './components/update';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AwsRum } from 'aws-rum-web';
+
+try {
+  const config = {
+    sessionSampleRate: 1,
+    guestRoleArn: "arn:aws:iam::751505345149:role/RUM-Monitor-us-east-1-751505345149-4082830924561-Unauth",
+    identityPoolId: "us-east-1:4be0d68a-2a27-453a-b9cd-1ad5a7215a08",
+    endpoint: "https://dataplane.rum.us-east-1.amazonaws.com",
+    telemetries: ["performance","errors","http"],
+    allowCookies: true,
+    enableXRay: false
+  };
+
+  const APPLICATION_ID = '730c1a22-17cd-46e9-a419-c09c7ea1029c';
+  const APPLICATION_VERSION = '1.0.0';
+  const APPLICATION_REGION = 'us-east-1';
+
+  const awsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  console.log("An error has occured")
+  console.log(error)
+}
+
 
 function App() {
   return (
