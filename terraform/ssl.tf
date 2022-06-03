49109a163487cead519e729a3cfde447b2d17100
Damien Gallagher <L00162772@student.lyit.ie>
2022-06-03 23:03:49 +0100

    Update

--- terraform/ssl.tf
+++ terraform/ssl.tf
@@ -31,7 +31,6 @@ resource "aws_acm_certificate" "alb_cert" {
 
 resource "aws_acm_certificate_validation" "alb_cert_validation" {
   certificate_arn         = aws_acm_certificate.alb_cert.arn
-  provider                = aws.east1
   validation_record_fqdns = [for record in aws_route53_record.alb_route53_validation_record : record.fqdn]
   depends_on = [
     aws_route53_record.alb_route53_validation_record
