be7c1093e2c7d85893a0b4b2388cd54334c626ec
Damien Gallagher <L00162772@student.lyit.ie>
2022-06-03 21:57:21 +0100

    Update

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -22,10 +22,28 @@ resource "aws_lb" "alb" {
   security_groups    = [module.lb_security_group.security_group_id]
 }
 
-resource "aws_lb_listener" "alb_http" {
+resource "aws_lb_listener" "alb_http_to_https" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "alb_https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.alb_cert.arn
 
   default_action {
     type = "redirect"
@@ -35,25 +53,8 @@ resource "aws_lb_listener" "alb_http" {
       status_code = "HTTP_301"
     }
   }
+
+  depends_on = [
+    aws_acm_certificate.alb_cert
+  ]
 }
-
-# resource "aws_lb_listener" "alb_https" {
-#   load_balancer_arn = aws_lb.alb.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = aws_acm_certificate.alb_cert.arn
-
-#   default_action {
-#     type = "redirect"
-
-#     redirect {
-#       host        = local.cloudfront_domain
-#       status_code = "HTTP_301"
-#     }
-#   }
-
-#   depends_on = [
-#     aws_acm_certificate.alb_cert
-#   ]
-# }
