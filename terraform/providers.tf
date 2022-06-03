c3b8bf2d71b8d4c04e540814f32e383b120faf0b
Damien Gallagher <L00162772@student.lyit.ie>
2022-06-03 22:12:02 +0100

    Update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -20,6 +20,16 @@ terraform {
 
 
 data "aws_caller_identity" "current" {}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.17.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
   #profile = "lyit"
