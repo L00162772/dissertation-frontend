bd964c80d0a9e497018071973bef91245339e8bd
Damien Gallagher <damien.gallagher@gmail.com>
2022-07-30 23:45:27 +0100

    Update

--- selenium/test_app.py
+++ selenium/test_app.py
@@ -4,6 +4,6 @@ import pytest
 @pytest.mark.usefixtures("setup")
 class TestExampleOne:
     def test_title(self):
-        self.driver.get('http://https://frontend.atu-dissertation.com')
+        self.driver.get('http://frontend.atu-dissertation.com')
         assert self.driver.title.startswith("React App - ")
 
