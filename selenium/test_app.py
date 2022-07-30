54c47af74897f269c2b4a225d9fdcd6ddaaf4f7f
Damien Gallagher <damien.gallagher@gmail.com>
2022-07-31 00:01:13 +0100

    Update

--- selenium/test_app.py
+++ selenium/test_app.py
@@ -5,5 +5,5 @@ import pytest
 class TestExampleOne:
     def test_title(self):
         self.driver.get('http://frontend.atu-dissertation.com')
-        assert self.driver.title.startswith("React Apps - ")
+        assert self.driver.title.startswith("React App - ")
 
